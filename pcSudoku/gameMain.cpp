442bcf41ab65f4c0fb9f5d71b662fd91046d3ade
amaorine <c0112548d8@edu.teu.ac.jp>
2015-06-18 23:26:58 +0900

    数字の被りを判定する関数の形だけ作った

--- pcSudoku/gameMain.cpp
+++ pcSudoku/gameMain.cpp
@@ -17,6 +17,62 @@ gameNumParts gameBord[2][9][9];
 
 //各関数とか
 
+
+
+//初期化時に使う,セットした数字が合っているかの判定
+int jnLine(int x,int y){
+	//num==gameBord[][x][y]になったとき0を返す。
+	//被ってなかったら1
+	//縦方向
+	//x変化なし、yが0～9、自身以外とかぶったら0
+	for(int i=0;i<9;i++){
+		if(i!=y){
+			//自分以外で縦で数字被ったら0
+			if(gameBord[0][x][y].num==gameBord[0][x][i].num){
+				return 0;
+			}
+		}
+	}
+	for(int i=0;i<9;i++){
+		if(i!=x){
+			//自分以外で縦で数字被ったら0
+			if(gameBord[0][x][y].num==gameBord[0][i][y].num){
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+	
+}
+int jnRange(int x,int y){
+	//x/3=0-2,y.3=0-2の3*3マス内での被り探し
+	//範囲設定
+	int xRange=x/3*3;
+	int yRange=y/3*3;
+
+	//判定
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(!(xRange+i==x&&yRange+j==y)){
+				//xyが自身以外の場合でかぶってたら0
+				if(gameBord[0][x][y].num==gameBord[0][xRange+i][yRange+i].num){
+					return 0;
+				}
+			}
+		}
+	}
+
+	return 1;
+}
+
+
+int judgeNum(int x,int y){
+	//縦・横・同じ3*3マス内で数字が被らなかったら1,被ったら0返す。
+	return jnLine(x,y)&&jnRange(x,y);
+}
+
+
 //初期化組
 void setNumberField(int dif){
 	//トリマ初期化
@@ -28,7 +84,16 @@ void setNumberField(int dif){
 		}
 	}
 
+	gameBord[0][0][0].num=1;
+
 	//答えの板を作る。gameBord[0][][]
+	for(int i=0;i<2;i++){
+		for(int j=0;j<9;j++){
+			//ランダムに数値を入れる
+			//ここから数独の条件に合ってるか判定
+
+		}
+	}
 
 
 
@@ -69,11 +134,16 @@ void drawField(){
 		DrawLine(xIni+i*lengthParts,yIni,xIni+i*lengthParts,yIni+length,GetColor(255,255,255)) ;
 	}
 		//数字描画
+		//とりあえずgameBord[0]のやつ描画してるけどこれは答え用なので描画すべきなのは[1]の方
 	for(int k=0;k<9;k++){
 		for(int j=0;j<9;j++){
 			DrawFormatString(xIni+k*lengthParts+lengthParts/2,yIni+j*lengthParts+lengthParts/2, GetColor(255,255,255),"%d",gameBord[0][k][j].num);
 		}
 	}
+	DrawFormatString( 240, 300, GetColor(255,255,255), "hjm:%d",judgeNum(0,0));
+	DrawFormatString( 240, 320, GetColor(255,255,255), "hjm:%d",jnLine(0,0));
+	DrawFormatString( 240, 330, GetColor(255,255,255), "hjm:%d",jnRange(0,0));
+	
 }
 
 
@@ -84,6 +154,10 @@ void drawField(){
 int gameMain(){
 	//ゲーム画面全体のメイン
 	//戻り値は画面切り替えしない限り必ず3
+	if(setIni==0){
+		setNumberField(getDifficulty());
+		setIni=1;
+	}
 
 
 	drawField();
